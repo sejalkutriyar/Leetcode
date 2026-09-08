4c1bdeef4067c4ba0a5b2c0a8347ced9f07a283b
👋 Hey there! I'm Sejal <seju020322@gmail.com>
2026-09-08 21:55:09 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,5 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        return Math.max(n - 999, 0);
+        if(n < 1000) {
+            return 0;
+        }
+        return n - 999;
     }
 }
