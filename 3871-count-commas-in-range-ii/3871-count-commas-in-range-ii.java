639641e4452f5688ca46d56d8239ee9368d2914a
👋 Hey there! I'm Sejal <seju020322@gmail.com>
2026-09-09 22:00:33 +0530

    Time: 1 ms (99.36%), Space: 42.9 MB (11.54%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long countCommas(long n) {
+        long curr = 1000;
+        long res = 0;
+
+        while (curr <= n) {
+            res += n - curr + 1;
+            curr *= 1000;
+        }
+
+        return res;
+    }
+}
