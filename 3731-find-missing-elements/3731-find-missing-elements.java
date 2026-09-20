004b67d4bce830c335b07efd98d15e11d54f6574
👋 Hey there! I'm Sejal <seju020322@gmail.com>
2026-09-21 00:49:14 +0530

    Time: 5 ms (46.38%), Space: 47 MB (44.74%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -4,16 +4,16 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         int min = nums[0];
         int max = nums[0];
-        for(int i = 0 ; i<nums.length;i++){
-            max = Math.max(max,nums[i]);
-            min = Math.min(min,nums[i]);
+        for(int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
             list.add(nums[i]);
         }
-        for(int i = min ; i<=max ; i++){
-            if(!list.contains(i)){
+        for(int i = min; i<=max; i++) {
+            if(!list.contains(i)) {
                 ans.add(i);
             }
         }
-        return ans;
+        return ans;   
     }
 }
