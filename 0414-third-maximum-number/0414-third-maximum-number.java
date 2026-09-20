a575d2d3b45703744b2ed7f20eafd706e12445a9
👋 Hey there! I'm Sejal <seju020322@gmail.com>
2026-09-20 20:08:52 +0530

    Time: 7 ms (28.81%), Space: 45.4 MB (20.88%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        Set<Integer> s = new HashSet<>();
+        for(int i = nums.length-1;i>=0;i--){
+            s.add(nums[i]);
+            if(s.size()==3){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
