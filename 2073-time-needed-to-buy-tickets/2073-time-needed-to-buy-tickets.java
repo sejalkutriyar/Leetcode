d0d64ab6537c35b09147cfbc5f4f2f5c30351bc2
👋 Hey there! I'm Sejal <seju020322@gmail.com>
2026-09-22 10:30:08 +0530

    Time: 11 ms (22.25%), Space: 46.5 MB (9.6%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,13 +1,27 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int time = 0;
+        Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i < tickets.length; i++) {
-            if(i <= k) {
-                time += Math.min(tickets[i], tickets[k]);
-            } else {
-                time += Math.min(tickets[i], tickets[k] - 1);
+            q.offer(i);
+        }
+        int time = 0;
+        while (!q.isEmpty()) {
+
+            int person = q.poll();
+
+            tickets[person]--;
+            time++;
+
+            if (person == k && tickets[person] == 0) {
+                return time;
+            }
+
+            if (tickets[person] > 0) {
+                q.offer(person);
             }
         }
+
         return time;
+        
     }
 }
