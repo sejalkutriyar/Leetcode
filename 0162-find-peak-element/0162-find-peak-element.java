20cf7c0523dc7c480ba1e9c2e0a4db45a48bd4f1
👋 Hey there! I'm Sejal <seju020322@gmail.com>
2026-09-26 00:03:14 +0530

    Time: 0 ms (100%), Space: 44.2 MB (57.48%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,15 +1,13 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int start = 0 ;
-        int end = nums.length-1;
-        while(start < end ){
-            int mid = start + (end-start)/2;
-            if(nums[mid] > nums[mid+1]){
-                end = mid;
-            }else{
-                start = mid+1;
+
+        for(int i = 0; i < nums.length - 1; i++) {
+
+            if(nums[i] > nums[i + 1]) {
+                return i;
             }
         }
-        return start ;
+
+        return nums.length - 1;
     }
 }
